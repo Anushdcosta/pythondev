5f69314f9df2363958d60a84568fb124373c9833
Anushdcosta <Anushviston@gmail.com>
2024-07-04 15:58:01 +0400

    first commit

---
+++ 1.py
@@ -0,0 +1,8 @@
+"""
+This script outputs a greeting message.
+It demonstrates the use of comments in Python,
+including multi-line and single-line comments.
+"""
+
+# Print a greeting message to the console
+print(f"Hello! How are you!")
---
+++ 3.py
@@ -0,0 +1,5 @@
+fruit1 = input("Enter the first fruit name: ")
+fruit2 = input("Enter the second fruit name: ")
+fruit3 = input("Enter the third fruit name: ")
+
+print(f"{fruit1}\n{fruit2}\n{fruit3}")

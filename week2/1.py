a85e0def3ae6e7b8220b2b608b06b728b61364a3
Anushdcosta <Anushviston@gmail.com>
2024-07-08 08:36:28 +0400

    week2

---
+++ week2/1.py
@@ -0,0 +1,5 @@
+line = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+line = line.upper()
+length = len(line)
+firstfive = line[0:5]
+print(firstfive)
---
+++ week2/3.py
@@ -0,0 +1,5 @@
+shopping = ["apple", "banana", "carrot", "lettuce", "biscuits"]
+shopping.append("milk")
+shopping.remove("carrot")
+shopping[2]
+print(shopping, "\n length = ", len(shopping))

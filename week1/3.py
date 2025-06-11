a72bd7753f9b3f69e3c10bfe3ec70c4f3a4d9154
Anushdcosta <anushviston@gmail.com>
2025-06-11 12:04:19 +0400

    clean old files and directories

--- week1/3.py
+++
@@ -1,5 +0,0 @@
-fruit1 = input("Enter the first fruit name: ")
-fruit2 = input("Enter the second fruit name: ")
-fruit3 = input("Enter the third fruit name: ")
-
-print(f"{fruit1}\n{fruit2}\n{fruit3}")
--- week2/3.py
+++
@@ -1,5 +0,0 @@
-shopping = ["apple", "banana", "carrot", "lettuce", "biscuits"]
-shopping.append("milk")
-shopping.remove("carrot")
-shopping[2]
-print(shopping, "\n length = ", len(shopping))
